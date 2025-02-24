f3e3a6212723ec071435815f281e99bbbb34aefd
Muhammad Mirab Br. <brojerdi84@gmail.com>
2025-02-24 22:28:08 +0330

    started working on the admin panel - login page

--- admin/views.py
+++ admin/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin import site
 
 # Create your views here.
+def adminView(request):
+    @login_required(login_url='/login/')
+    def adminView(request):
+        return render(request, 'admin/home.html', {'site': site})
